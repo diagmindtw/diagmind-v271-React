f6b73aa5a4ba2e58d29eeaf00fd83b2f0332fbcd
diagmindtw <diagmindtw@gmail.com>
2026-03-14 20:59:28 +0800

    Commit from TeleportHQ

---
+++ src/views/frame.js
@@ -0,0 +1,389 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './frame.css'
+
+const Frame = (props) => {
+  return (
+    <div className="frame-container">
+      <Helmet>
+        <title>exported project</title>
+        <meta property="og:title" content="exported project" />
+        <link
+          rel="canonical"
+          href="https://diagmind-v271-jkjzzs.teleporthq.app/"
+        />
+        <meta
+          property="og:url"
+          content="https://diagmind-v271-jkjzzs.teleporthq.app/"
+        />
+      </Helmet>
+      <div className="frame-thq-frame-elm">
+        <span className="frame-thq-text-elm10">熱門回應</span>
+        <div className="frame-thq-frame1321317464-elm">
+          <div className="frame-thq-frame1321317457-elm">
+            <div className="frame-thq-cards-elm1">
+              <div className="frame-thq-customer-quote-elm1">
+                <span className="frame-thq-text-elm11">
+                  “看不懂，求詳細解釋”
+                </span>
+                <div className="frame-thq-avatar-elm1">
+                  <img
+                    alt="Avatar1911"
+                    src="/avatar1911-fi1d-200w.png"
+                    className="frame-thq-avatar-elm2"
+                  />
+                  <div className="frame-thq-frame2610301-elm1">
+                    <span className="frame-thq-text-elm12 Smalltext">
+                      高瑞璋
+                    </span>
+                    <span className="frame-thq-text-elm13 Smalltext">
+                      於 Eisenmenger syndrome 留言
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-thq-customer-quote-elm2">
+                <span className="frame-thq-text-elm14">
+                  “看不懂，求詳細解釋”
+                </span>
+                <div className="frame-thq-avatar-elm3">
+                  <img
+                    alt="Avatar1918"
+                    src="/avatar1918-5g3d-200h.png"
+                    className="frame-thq-avatar-elm4"
+                  />
+                  <div className="frame-thq-frame2610301-elm2">
+                    <span className="frame-thq-text-elm15 Smalltext">
+                      高端璋
+                    </span>
+                    <span className="frame-thq-text-elm16 Smalltext">
+                      於 人造瓣膜的選擇 Prosthetic valve 留言
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="frame-thq-customer-quote-elm3">
+                <span className="frame-thq-text-elm17">
+                  “看不懂，求詳細解釋”
+                </span>
+                <div className="frame-thq-avatar-elm5">
+                  <img
+                    alt="Avatar1925"
+                    src="/avatar1925-w68m-200h.png"
+                    className="frame-thq-avatar-elm6"
+                  />
+                  <div className="frame-thq-frame2610301-elm3">
+                    <span className="frame-thq-text-elm18 Smalltext">
+                      高瑞璋
+                    </span>
+                    <span className="frame-thq-text-elm19 Smalltext">
+                      於 immune thrombocytopenic  purpura 留言
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-thq-hero-image-elm">
+          <div className="frame-thq-copy-elm1">
+            <div className="frame-thq-pagetitle-elm">
+              <span className="frame-thq-text-elm20">Diagmind Taiwan</span>
+              <span className="frame-thq-text-elm21">
+                <span>Digitizing Medical Knowledge, from Taiwan</span>
+                <br></br>
+                <span>
+                  Diagmind Taiwan makes studying a breeze and life during
+                  clinical experience easier
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+              </span>
+            </div>
+            <button className="frame-thq-button-elm1">
+              <span className="frame-thq-text-elm24">Log in</span>
+            </button>
+          </div>
+        </div>
+        <div className="frame-thq-frame1321317462-elm">
+          <div className="frame-thq-frame1321317461-elm">
+            <div className="frame-thq-frame1321317459-elm">
+              <span className="frame-thq-text-elm25">各分科主題知識</span>
+            </div>
+            <div className="frame-thq-frame1321317460-elm">
+              <div className="frame-thq-frame1321317458-elm">
+                <div className="frame-thq-frame1321317456-elm">
+                  <div className="frame-thq-card-elm1">
+                    <div className="frame-thq-image-elm1"></div>
+                    <div className="frame-thq-copy-elm2">
+                      <span className="frame-thq-text-elm26">
+                        醫學三
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <span className="frame-thq-text-elm27">
+                        包括內科、家庭醫學科等科目及其相關臨床實例與醫學倫理
+                      </span>
+                    </div>
+                  </div>
+                  <div className="frame-thq-card-elm2">
+                    <div className="frame-thq-image-elm2"></div>
+                    <div className="frame-thq-copy-elm3">
+                      <span className="frame-thq-text-elm28">
+                        醫學五
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <span className="frame-thq-text-elm29">
+                        包括外科、骨科、泌尿科等科目及其相關臨床實例與醫學倫理
+                      </span>
+                    </div>
+                  </div>
+                  <div className="frame-thq-card-elm3">
+                    <div className="frame-thq-image-elm3"></div>
+                    <div className="frame-thq-copy-elm4">
+                      <span className="frame-thq-text-elm30">
+                        醫學六
+                        <span
+                          dangerouslySetInnerHTML={{
+                            __html: ' ',
+                          }}
+                        />
+                      </span>
+                      <span className="frame-thq-text-elm31">
+                        包括麻醉科、眼科、耳鼻喉科、婦產科、復健科等科目及醫學倫理
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-thq-image-elm4"></div>
+        <div className="frame-thq-frame1321317466-elm">
+          <span className="frame-thq-text-elm32">熱門主題與解答</span>
+        </div>
+        <div className="frame-thq-frame1321317465-elm">
+          <div className="frame-thq-cards-elm2">
+            <div className="frame-thq-card-elm4">
+              <div className="frame-thq-image-elm5"></div>
+              <div className="frame-thq-copy-elm5">
+                <span className="frame-thq-text-elm33">
+                  Eisenmenger syndrome
+                </span>
+                <span className="frame-thq-text-elm34">
+                  A severe complication of certain congenital heart defects
+                  where increased blood flow to the lungs leads to high blood
+                  pressure in the pulmonary arteries.
+                </span>
+              </div>
+            </div>
+            <div className="frame-thq-card-elm5">
+              <div className="frame-thq-image-elm6"></div>
+              <div className="frame-thq-copy-elm6">
+                <span className="frame-thq-text-elm35">
+                  人造瓣膜的選擇 Prosthetic valve
+                </span>
+                <span className="frame-thq-text-elm36">
+                  <span>
+                    人造瓣膜的選擇主要有生物瓣膜和機械瓣膜兩種，選擇時需考慮患者年齡、生活方式、以及對抗凝血劑的耐受性等因素。
+                  </span>
+                  <br></br>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <span className="frame-thq-text-elm38">今日主題: 兒童心臟科</span>
+        <div className="frame-thq-frame1321317470-elm">
+          <div className="frame-thq-frame1321317468-elm">
+            <div className="frame-thq-text-elm39">
+              <div className="frame-thq-text-elm40">
+                <span className="frame-thq-text-elm41">
+                  Eisenmenger syndrome
+                </span>
+                <span className="frame-thq-text-elm42">
+                  A severe complication of certain congenital heart defects
+                  where increased blood flow...
+                </span>
+              </div>
+              <div className="frame-thq-text-elm43">
+                <span className="frame-thq-text-elm44">
+                  Total Anomalous Pulmonary Venous Return
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: ' ',
+                    }}
+                  />
+                </span>
+                <span className="frame-thq-text-elm45">
+                  一種先天性心臟疾病，指的是肺靜脈沒有正常連接到左心房，而是連接到右心房或其他地方。
+                </span>
+              </div>
+              <div className="frame-thq-text-elm46">
+                <span className="frame-thq-text-elm47">主動脈弓中斷</span>
+                <span className="frame-thq-text-elm48">
+                  <span>主動脈弓與降主動脈中斷，靠PDA連接降主動脈</span>
+                  <br></br>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="frame-thq-frame1321317467-elm">
+          <button className="frame-thq-buttons-elm1">
+            <button className="frame-thq-button-elm2">
+              <span className="frame-thq-text-elm50">Last Topic</span>
+            </button>
+            <button className="frame-thq-button-elm3">
+              <span className="frame-thq-text-elm51">Next Topic</span>
+            </button>
+          </button>
+        </div>
+        <div className="frame-thq-section-elm">
+          <span className="frame-thq-text-elm52">Homepage</span>
+          <button className="frame-thq-buttons-elm2">
+            <button className="frame-thq-button-elm4">
+              <span className="frame-thq-text-elm53">
+                Log in
+                <span
+                  dangerouslySetInnerHTML={{
+                    __html: ' ',
+                  }}
+                />
+              </span>
+            </button>
+            <button className="frame-thq-button-elm5">
+              <span className="frame-thq-text-elm54">Sign up</span>
+            </button>
+          </button>
+        </div>
+        <div className="frame-thq-navigation-footer-elm">
+          <div className="frame-thq-items-elm1">
+            <span className="frame-thq-text-elm55 Smalltext">Topic</span>
+            <span className="frame-thq-text-elm56 Smalltext">Page</span>
+            <span className="frame-thq-text-elm57 Smalltext">Page</span>
+            <span className="frame-thq-text-elm58 Smalltext">Page</span>
+          </div>
+          <div className="frame-thq-items-elm2">
+            <span className="frame-thq-text-elm59 Smalltext">Topic</span>
+            <span className="frame-thq-text-elm60 Smalltext">Page</span>
+            <span className="frame-thq-text-elm61 Smalltext">Page</span>
+            <span className="frame-thq-text-elm62 Smalltext">Page</span>
+          </div>
+          <div className="frame-thq-items-elm3">
+            <span className="frame-thq-text-elm63 Smalltext">Topic</span>
+            <span className="frame-thq-text-elm64 Smalltext">Page</span>
+            <span className="frame-thq-text-elm65 Smalltext">Page</span>
+            <span className="frame-thq-text-elm66 Smalltext">Page</span>
+          </div>
+          <span className="frame-thq-text-elm67">Diagmind Taiwan</span>
+          <div className="frame-thq-social-icons-elm">
+            <button className="frame-thq-buttons-icon-elm1">
+              <div className="frame-thq-icon-elm1">
+                <img
+                  alt="Icon1318"
+                  src="/icon1318-jz0j.svg"
+                  className="frame-thq-icon-elm2"
+                />
+              </div>
+            </button>
+            <button className="frame-thq-buttons-icon-elm2">
+              <div className="frame-thq-icon-elm3">
+                <img
+                  alt="Icon1319"
+                  src="/icon1319-h7gk.svg"
+                  className="frame-thq-icon-elm4"
+                />
+              </div>
+            </button>
+            <button className="frame-thq-buttons-icon-elm3">
+              <div className="frame-thq-icon-elm5">
+                <img
+                  alt="Icon1319"
+                  src="/icon1319-5dyag.svg"
+                  className="frame-thq-icon-elm6"
+                />
+              </div>
+            </button>
+            <button className="frame-thq-buttons-icon-elm4">
+              <div className="frame-thq-icon-elm7">
+                <img
+                  alt="Icon1320"
+                  src="/icon1320-c0c.svg"
+                  className="frame-thq-icon-elm8"
+                />
+              </div>
+            </button>
+          </div>
+          <img
+            alt="Divider1320"
+            src="/divider1320-c5l.svg"
+            className="frame-thq-divider-elm"
+          />
+        </div>
+        <div className="frame-thq-navigation-elm">
+          <button className="frame-thq-button-elm6">
+            <span className="frame-thq-text-elm68">登入</span>
+          </button>
+          <div className="frame-thq-group1-elm">
+            <span className="frame-thq-text-elm69">分科</span>
+            <span className="frame-thq-text-elm70">主題</span>
+            <span className="frame-thq-text-elm71">題目</span>
+          </div>
+          <img
+            alt="logo11492"
+            src="/logo11492-bsx-200h.png"
+            className="frame-thq-logo1-elm"
+          />
+          <img
+            alt="logo31492"
+            src="/logo31492-pq22-200h.png"
+            className="frame-thq-logo3-elm"
+          />
+          <div className="frame-thq-rectangle1-elm1">
+            <img
+              alt="Rectangle1I149"
+              src="/rectangle1i149-jf0q-1500w.png"
+              className="frame-thq-rectangle1-elm2"
+            />
+          </div>
+          <div className="frame-thq-searchbar-elm">
+            <div className="frame-thq-statelayer-elm1">
+              <div className="frame-thq-content-elm1">
+                <span className="frame-thq-text-elm72 M3bodylarge">搜尋</span>
+              </div>
+              <div className="frame-thq-trailing-elements-elm">
+                <div className="frame-thq-frame1sttrailingicon-elm">
+                  <div className="frame-thq-content-elm2">
+                    <div className="frame-thq-statelayer-elm2">
+                      <img
+                        alt="IconI149"
+                        src="/iconi149-88n6.svg"
+                        className="frame-thq-icon-elm9"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Frame
